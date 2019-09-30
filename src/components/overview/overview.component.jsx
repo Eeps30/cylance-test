185dd3910699adf200f212941ee30cc0b6568c84
Eeps30 <eeps30@gmail.com>
2019-09-30 16:44:24 -0700

    successfully lazy loading Overview tab. Will most likely change lazy loading over to Repositories component later

--- src/components/overview/overview.component.jsx
+++ src/components/overview/overview.component.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Overview = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('https://api.github.com/users/octocat');
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log('data from call', data);
+
   return <div>Overview Component</div>;
 };
 
--- src/pages/landing-page/landing.component.jsx
+++ src/pages/landing-page/landing.component.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import Container from 'react-bootstrap/Container';
-import Overview from '../../components/overview/overview.component';
 import Repos from '../../components/repos/repos.component';
+const Overview = React.lazy(() =>
+  import('../../components/overview/overview.component')
+);
 
 const Landing = () => {
   return (
     <Container>
-      <Tabs defaultActiveKey='profile' id='uncontrolled-tab-example'>
+      <Tabs
+        defaultActiveKey='profile'
+        id='uncontrolled-tab-example'
+        animation='true'
+        mountOnEnter={true}
+        unmountOnExit={false}
+      >
         <Tab eventKey='home' title='Overview'>
-          <Overview />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Overview />
+          </Suspense>
         </Tab>
         <Tab eventKey='profile' title='Repositories'>
           <Repos />
